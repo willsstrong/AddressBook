a394b387b654495bb434a15952b248dc495d16ec
Will Strong <38888375+willsstrong@users.noreply.github.com>
2019-11-26 00:46:42 -0700

    Enabled Migrations and deployed to Azure account: http://addressindex.azurewebsites.net/

---
+++ Migrations/201911260733299_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿namespace AddressBook.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.People",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        LastName = c.String(),
+                        FirstName = c.String(),
+                        Address = c.String(),
+                        City = c.String(),
+                        Province = c.String(),
+                        PostCode = c.String(),
+                        PhoneNumMain = c.String(),
+                        PhoneNumCell = c.String(),
+                        Email = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.People");
+        }
+    }
+}
---
+++ Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+﻿namespace AddressBook.Migrations
+{
+    using AddressBook.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<AddressBook.DAL.AddressContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(AddressBook.DAL.AddressContext context)
+        {
+            var person = new List<Person>
+            {
+                new Person { FirstName = "Will",   LastName = "Strong"},
+                new Person { FirstName = "Mike",   LastName = "Anderson" },
+                new Person { FirstName = "Stephanie",   LastName = "Lee" },
+                new Person { FirstName = "Linda",   LastName = "Zacharius" }
+
+            };
+            person.ForEach(s => context.Person.AddOrUpdate(p => p.LastName));
+            context.SaveChanges();
+        }
+    }
+}
